f092572a84e67c95d695e0e8f4de74d8b507c2ee
Artem Mamichev <70771092+artem-mamichev@users.noreply.github.com>
2022-11-21 04:03:39 +0100

    Test works for one picture! Added Pictures folder

--- Analysis_with_convol/GetProfileROIFit2_25_forv2_TO_MODIFY.m
+++ Analysis_with_convol/GetProfileROIFit2_25_forv2_TO_MODIFY.m
@@ -56,52 +56,50 @@ WTH = 3;
     %-------------------------------------------------------------------------
 
     % Image folder : gain 1 
-[fn1 pn1] = uigetfile('*.png');
-pn1 = pn1(1 : length(pn1) - 1);
-cd(pn1);
+% Instead of chosing the image every time it's faster to declare it's path
+% file = "/Users/spark/Desktop/Pictures";  % Here just paste a Picture file path 
+% [pn, ~, ~] = fileparts(file);  % pn1: path
+
+fprintf("CHOSE THE OpenReader PROJECT FOLDER!");
+proj_path = uigetdir();  % Choose directory of OpenReader project folder
+
+pn = proj_path + "/Pictures";
+cd(pn);
 a1 = dir('*.png');
 N1 = numel(a1);
 warning off;
 pause on;
-
-pn = pn1(1 : length(pn1) - 2);% ROMAIN: SEEMS NOT TO WORK
-cd(pn); 
 
 % Creation of the.txt file
 
 cd(pn);
 % Type of assay = 'Simplex'
   Title = [
-    'Name',    ';', 'T1',     ';', 'C',     ';', 'BG_T1',    ';',
-    'BG_C',    ';', 'R�_T1',  ';', 'R�_C',  ';', 'Width_T1', ';',
-    'Width_C', ';', 'T/BG_1', ';', 'Gain1', ';', 'ROI1'
+    'Name',    ';', 'T1',     ';', 'C',     ';', 'BG_T1',    ';', 'BG_C',    ';', 'R�_T1',  ';', 'R�_C',  ';', 'Width_T1', ';', 'Width_C', ';', 'T/BG_1', ';', 'Gain1', ';', 'ROI1'
   ];
 
-fs=find(pn=='\');
-Nomp=pn(fs(length(fs)-1)+1:fs(length(fs))-1);
+% Opening results txt file for pic 1
+fs = a1(1).name;
+Nomp=fs(strfind(fs, 'N') + 1 : strfind(fs, '.') - 1);  % image number (sybbols from 'N' to '.' in the image name)
 NomAna=['AnalysisResults' Nomp '_' Col '.txt'];
 fileID=fopen(NomAna,'w');
 fprintf(fileID,Title);
 fprintf(fileID,'\r\n');
 NomA=[];
 
-% Loop for all images in the folder
-for i=1:N1
-   Nom1=a1(i).name;
+% First image
+Nom1=a1(1).name;
 
    
 % Load images of folder 1
-cd(pn1);
-Im1=double(imread(Nom1));
+Im1=double(imread(Nom1));  % pixels array n x m x 3
 % Col = 'RGB'
 A1=(Im1(:,:,1)+Im1(:,:,2)+Im1(:,:,3))/3;
 
-%comment these 2 lines if using with v1 reader
-A1 = transpose(A1);
-A1 = flipud(A1);
+%comment these 2 lines if using with v1 reader 
+A1 = transpose(A1);  
+A1 = flipud(A1);  % We use v2
    
-% Load images of folder 2
-cd(pn)
 %-------------------------------------------------------------------------
 
 % Selection of the appropriate image
@@ -111,14 +109,15 @@ cd(pn)
 
 
 % Image parameters : image 1
-m1=min(min(A1));
+m1=min(min(A1));  % M = min(A) returns the minimum elements of an array.
 M1=max(max(A1));
 S1=size(A1);
-h1=S1(1);
-w1=S1(2);
+h1=S1(1);  % number of raws
+w1=S1(2);  % number of columns
 
 
 %Display image 1
+
 imshow(A1,[m1 M1]);
 
 
@@ -155,7 +154,7 @@ pause(0.7);
 %-------------------------------------------------------------------------
 
 % Bogdana modified up to here
-
+cd(proj_path + "/Analysis_with_convol");
 % Image selection
 
 I_T1_Raw=I1_T1;
@@ -219,8 +218,8 @@ beta0_C=[bg_C d_C 1 posM_C sqrt(2*N2) ];
 % Fit without constraint
 GaussTrend = @(par,x)  par(1) + (x-10)*par(2) + par(3)*exp(-((x-par(4)).^2)/par(5)^2); 
 
-[beta_T1 r_T1 J_T1]=nlinfit(Px_trunc,Profil_T1_trunc/M_T1,GaussTrend,beta0_T1);
-[beta_C r_C J_C]=nlinfit(Px_trunc,Profil_C_trunc/M_C,GaussTrend,beta0_C);
+[beta_T1, r_T1, J_T1]=nlinfit(Px_trunc,Profil_T1_trunc/M_T1,GaussTrend,beta0_T1);
+[beta_C, r_C, J_C]=nlinfit(Px_trunc,Profil_C_trunc/M_C,GaussTrend,beta0_C);
 
 fit_T1=M_T1*GaussTrend(beta_T1,Px)+m_T1;
 fit_C=M_C*GaussTrend(beta_C,Px)+m_C;
@@ -333,24 +332,24 @@ close all;
 
 
 % Save results in a structure
-Res(i).Name=Nom1;
+Res(1).Name=Nom1;
 
-Res(i).Test1=Si_1;
-Res(i).Control=C;
+Res(1).Test1=Si_1;
+Res(1).Control=C;
 
-Res(i).BG_T1=Bg_T1;
-Res(i).BG_C=Bg_C;
+Res(1).BG_T1=Bg_T1;
+Res(1).BG_C=Bg_C;
 
-Res(i).R2_T=R2_T1;
-Res(i).R2_C=R2_C;
+Res(1).R2_T=R2_T1;
+Res(1).R2_C=R2_C;
 
-Res(i).WidthT1=W_T1;
-Res(i).WidthC=W_C;
+Res(1).WidthT1=W_T1;
+Res(1).WidthC=W_C;
 
 %Alternatively Strip over Control
-Res(i).T_BG_T1=Si_1/Bg_T1;
-Res(i).N_gain1=Gain1;
-Res(i).ROI1=ROIvalue_1;
+Res(1).T_BG_T1=Si_1/Bg_T1;
+Res(1).N_gain1=Gain1;
+Res(1).ROI1=ROIvalue_1;
 
 
 
@@ -397,9 +396,10 @@ fprintf(fileID,'%d',ROIvalue_1);
 fprintf(fileID,'\r\n');
 
 
-end
-fclose('all');
-close all;
+
+fclose(fileID);
+%close all;
 
 
 Resu=Res;
+end
